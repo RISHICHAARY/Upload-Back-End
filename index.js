5a82071f1757e4b787faae66ecbb9c855a31af86
Rishi <rs3715@srmist.edu.in>
2023-01-19 13:45:09 +0530

    HI

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://Rishichaary:rishi12345@codemath.lfw6dor.mongodb.net/?retryWrites=true&w=majority", 
+mongoose.connect("mongodb://Rishichaary:rishi1234@ac-yx8v9oa-shard-00-00.por1vin.mongodb.net:27017,ac-yx8v9oa-shard-00-01.por1vin.mongodb.net:27017,ac-yx8v9oa-shard-00-02.por1vin.mongodb.net:27017/?ssl=true&replicaSet=atlas-qm83pl-shard-0&authSource=admin&retryWrites=true&w=majority", 
 {
     useNewUrlParser:true,
 });
@@ -119,6 +119,6 @@ app.put("/deleteMe" , (req , res)=>{
 
 //---------------------------------------------------------------Server----------------------------------------------------------------------------
 
-app.listen(() => {
+app.listen(3001 , () => {
     console.log("Server On");
 } );
